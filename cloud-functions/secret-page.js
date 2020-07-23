93edd5641c975f20c648b6ae293b6e1114c02874
Ariunsanaa <ariunsanaa.9161@gmail.com>
2020-07-22 20:00:44 -0700

    cloud function hiine

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,30 @@
+/*
+ * ene function ni server side deer ajillana
+ * @param - event : hereglegchees irj bga requist
+ *          callback : ajiluulah function
+ *          context : unknown
+ */
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "welcome to secret page",
-  });
+  let body;
+  const html = `
+  <h2>Hello this is secret page</h2>
+  <p>yu bn zaluu meen</p>
+  `;
+  // herev hereglegchees irj bui requist ymar neg body buyu ugugdultei tsug bval
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  // herev hereglegchees irsen ugugdul taarval butsaagaad ugugdul ilgeene
+  if (body.password === "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: html,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
